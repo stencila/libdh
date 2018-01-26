d60e80f13b30bd5ecc683c68aa50105abe2d6312
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-01-26 19:21:34 +1300

    Stub for generation script

---
+++ generate_doc.py
@@ -0,0 +1,3 @@
+# Generates documentation for the functions included in the `functions` directory
+# Documentation is generated in Markdown format and placed in the `doc` directory
+# GitHub will automatically display this documentation in the repository GitHub pages
