34bbb4ea64cf7c46b15c55a19b65734932a32f77
Nokome Bentley <nokome@stenci.la>
2018-03-20 13:26:19 +1300

    Fix tests

--- tests/ngram_test.py
+++ tests/ngram_test.py
@@ -9,10 +9,9 @@ def test_ngram_string():
     assert grams == [
         ('world hello', 2),
         ('hello world', 2),
-        ('hello alice', 1),
-        ('alice and', 1),
-        # Seems to an issue with the function, currently not including last word/phrase
         ('and bob', 1),
+        ('hello alice', 1),
+        ('alice and', 1)
     ]
 
 
@@ -20,9 +19,7 @@ def test_ngram_array_string():
     """
     ngram can be called with an array of strings (e.g. a column of cells containing strings)
     """
-    grams = ngram(['Hello world.', 'Hello, world!', 'Hello,..world?'])
+    grams = ngram(['Hello world.', 'Hello, world!', 'Hello... world?'])
     assert grams == [
-        # Due to issue noted above, this is currently returning an empty list 
-        # instead of the following?
         ('hello world', 3)
     ]
